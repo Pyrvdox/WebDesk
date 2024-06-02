de832c24260d1623aedc4263b8fb04f8dc91c356
Pyrvdox <cierech8991@gmail.com>
2024-06-02 23:01:00 +0200

    new note style

--- frontend/src/Components/Newnote.js
+++ frontend/src/Components/Newnote.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import NavBar from './Navbar';
+import '../Styles/singlenotestyle.css'
 
 const Newnote = () => { 
+
     return(
         <>
-            
+            <NavBar />
+            <section className="note-section">
+                <div className="note-box">
+                <form>
+                    <div className="note-header">
+                        <input 
+                            type="text"  
+                            className="note-title-input"
+                        />
+                        <button>Save</button>
+                    </div>
+                    <div className="note-body">
+                        <textarea  
+                            className="note-textarea"
+                        />
+                    </div>
+                </form>
+                </div>
+            </section>
         </>
     )
 }
