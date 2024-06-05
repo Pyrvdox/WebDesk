eedf34e159d43cf521af9c11d9905aa53112ede8
Pyrvdox <cierech8991@gmail.com>
2024-06-05 10:45:44 +0200

    Calculator States

--- frontend/src/Components/Calculator.js
+++ frontend/src/Components/Calculator.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./Navbar";
 import '../Styles/calcstyle.css'
 
 const CalculatorComponent = () => {
+
+    const [equation, setEquation] = useState({calc:''})
+    const [resultInfo, setResultInfo] = useState({result:''})
+
+    const handleChange = (e) => {
+        setEquation({
+          ...equation,
+          [e.target.name]: e.target.value,
+        });
+      };
+
+    const handleSubmit = () => {
+        console.log(equation)
+        setResultInfo(equation.calc)
+        console.log(resultInfo)
+        setEquation({calc:''})
+    }
+
+    const handleReset = () => {
+        setEquation({calc:''})
+    }
+
     return (
         <>
         <NavBar/>
@@ -10,15 +32,15 @@ const CalculatorComponent = () => {
         <h1 className="calc-title">Calculator</h1>
             <div className="calc-wrapper">
                 <div className="calc-header">
-                    <input className="calc-input" placeholder="Enter your equation"></input>
+                    <input className="calc-input" placeholder="Enter your equation" name='calc' value={equation.calc} onChange={handleChange}></input>
                 </div>
                 <div className="calc-results">
                     <h2>Result: </h2>
-                    <h2></h2>
+                    <h2>{resultInfo}</h2>
                 </div>
                 <div className="calc-body">
-                    <button className="calc-button reset-calc-button">Reset</button>
-                    <button className="calc-button">Enter</button> 
+                    <button className="calc-button reset-calc-button" onClick={handleReset}>Reset</button>
+                    <button className="calc-button" onClick={handleSubmit}>Enter</button> 
                 </div>
 
             </div>
