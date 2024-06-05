9ee4a64cad2c87348b233b7ed43764ad6341790c
Pyrvdox <cierech8991@gmail.com>
2024-06-05 10:28:50 +0200

    Calculator Component style 2

--- frontend/src/Components/Calculator.js
+++ frontend/src/Components/Calculator.js
@@ -10,9 +10,17 @@ const CalculatorComponent = () => {
         <h1 className="calc-title">Calculator</h1>
             <div className="calc-wrapper">
                 <div className="calc-header">
-                    <input className="calc-input" placeholder="0.0"></input>
+                    <input className="calc-input" placeholder="Enter your equation"></input>
                 </div>
-                
+                <div className="calc-results">
+                    <h2>Result: </h2>
+                    <h2></h2>
+                </div>
+                <div className="calc-body">
+                    <button className="calc-button reset-calc-button">Reset</button>
+                    <button className="calc-button">Enter</button> 
+                </div>
+
             </div>
         </section>
         </>
