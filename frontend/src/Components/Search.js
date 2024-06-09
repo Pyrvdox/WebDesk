1c25b52878da0e22c44802a203ad46fefbb10898
Pyrvdox <cierech8991@gmail.com>
2024-06-09 22:52:36 +0200

    search function
    working search function

--- frontend/src/Components/Search.js
+++ frontend/src/Components/Search.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from "./Navbar";
 import '../Styles/searchstyle.css'
 import Loupe from '../Assets/loupe.png'
 
 const SearchComponent = () => {
 
-    const searchHandler = () => {
-        console.log("click")
+    const [searchLink, setSearchLink] = useState('')
+
+    const handleChange = (e) => {
+        setSearchLink({
+          ...searchLink,
+          [e.target.name]: e.target.value,
+        });
+      };
+
+    const searchHandler = (e) => {
+        e.preventDefault();
+
+        let link = String(searchLink.link); 
+
+        if(link == ''){
+            return
+        }
+        if (!link.startsWith('http://') && !link.startsWith('https://')) {
+            link = 'http://' + link;
+        }
+        window.location.href = link;
     }
 
     return(
@@ -15,7 +34,7 @@ const SearchComponent = () => {
             <section className='search-box'>
                 <h1>Web Search</h1>
                 <div className='search-wrapper'>
-                    <input className='search-input-field' placeholder='Type or paste the web address'></input>
+                    <input className='search-input-field' placeholder='Type or paste the web address' type='link' name='link' onChange={handleChange}></input>
                     <button className='search-button' onClick={searchHandler}><img src={Loupe} alt="Loupe" className='search-loupe'/></button>
                 </div>
             </section>
