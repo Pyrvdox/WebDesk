2ef684974f593d9127ef8b9e1db6568bcbafb258
Pyrvdox <cierech8991@gmail.com>
2024-06-09 22:32:45 +0200

    search style 2

--- frontend/src/Components/Search.js
+++ frontend/src/Components/Search.js
@@ -5,14 +5,18 @@ import Loupe from '../Assets/loupe.png'
 
 const SearchComponent = () => {
 
+    const searchHandler = () => {
+        console.log("click")
+    }
+
     return(
         <>
             <NavBar />
             <section className='search-box'>
                 <h1>Web Search</h1>
                 <div className='search-wrapper'>
-                    <input className='search-input-field'></input>
-                    <button className='search-button'><img src={Loupe} alt="Loupe" className='search-loupe'/></button>
+                    <input className='search-input-field' placeholder='Type or paste the web address'></input>
+                    <button className='search-button' onClick={searchHandler}><img src={Loupe} alt="Loupe" className='search-loupe'/></button>
                 </div>
             </section>
         </>
