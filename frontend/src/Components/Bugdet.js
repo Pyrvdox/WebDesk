6f50dc41a4ca8335d10692fad5f3fb56b3e1802f
Pyrvdox <cierech8991@gmail.com>
2024-06-25 20:04:43 +0200

    classNames added

--- frontend/src/Components/Bugdet.js
+++ frontend/src/Components/Bugdet.js
@@ -7,9 +7,21 @@ const BudgetComponent = () => {
         <>
         <NavBar />
         <section className="b-section">
-            <p>Hello</p>
-        </section>
+        <h1 className="budget-title">My Budget</h1>
+        <h3 className="budget-subtitle"Enter the basic details of your income and expenses</h3>
+        <div className="budget-wrapper">
+            <div className="budget-form">
+                <div className="budget-container">
 
+                </div>
+            </div>
+            <div className="budget-results">
+                <div className="budget-container">
+
+                </div>
+            </div>
+        </div>  
+        </section>
         </>
     )
 }
