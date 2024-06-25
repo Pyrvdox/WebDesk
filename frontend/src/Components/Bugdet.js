161a5c9c9fc1bbb80654f1c8cfaa0bbc845d76e0
Pyrvdox <cierech8991@gmail.com>
2024-06-25 21:19:33 +0200

    formHandler

--- frontend/src/Components/Bugdet.js
+++ frontend/src/Components/Bugdet.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./Navbar";
 import '../Styles/bugdetstyle.css'
 
+
 const BudgetComponent = () => {
+
+    const [budgetData, setBudgetData] = useState({
+        income:"",
+        commitments:"",
+        house:"",
+        othercommitments:"",
+        other:""
+    })
+
+    const handleChange = (e) => {
+        setBudgetData({
+          ...budgetData,
+          [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(budgetData)
+    }
+
     return(
         <>
         <NavBar />
@@ -13,25 +35,18 @@ const BudgetComponent = () => {
             <div className="budget-form">
                 <div className="budget-container">
                 <form className="budget-form">
-                    <labe>Income</labe>
-                    <input></input>
-                    <labe>Commitments (rent etc.)</labe>
-                    <input></input>
-                    <labe>Food and housekeeping</labe>
-                    <input></input>
-                    <labe>Other commitments (car, phone, internet)</labe>
-                    <input></input>
-                    <labe>Food and housekeeping</labe>
-                    <input></input>
-                    <labe>Other expenses</labe>
-                    <input></input>
-                    <button type="submit">Submit</button>
+                    <label>Income</label>
+                    <input value={budgetData.income} onChange={handleChange} name="income"></input>
+                    <label>Commitments (rent etc.)</label>
+                    <input value={budgetData.commitments} onChange={handleChange} name="commitments"></input>
+                    <label>Food and housekeeping</label>
+                    <input value={budgetData.house} onChange={handleChange} name="house"></input>
+                    <label>Other commitments (car, phone, internet)</label>
+                    <input value={budgetData.othercommitments} onChange={handleChange} name="othercommitments"></input>
+                    <label>Other expenses</label>
+                    <input value={budgetData.other} onChange={handleChange} name="other"></input>
+                    <button type="submit" onClick={handleSubmit}>Submit</button>
                 </form>
-                </div>
-            </div>
-            <div className="budget-results">
-                <div className="budget-container">
-
                 </div>
             </div>
         </div>  
